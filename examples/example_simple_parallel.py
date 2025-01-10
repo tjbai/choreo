00a9b0bf45b2c7ccbdd5cd62b6e8dc236091a133
TJ Bai <tjianbai@gmail.com>
2025-01-09 21:34:01 -0500

    update example

--- examples/example_simple_parallel.py
+++ examples/example_simple_parallel.py
@@ -1,5 +1,5 @@
+import os
 import fire
-
 from llama import Workflow
 
 def main(
@@ -11,44 +11,42 @@ def main(
     max_batch_size: int = 4,
     max_gen_len: int = 128,
 ):
+    os.environ["RANK"] = "0"
+    os.environ["WORLD_SIZE"] = "1"
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "29500"
+
     workflow = Workflow.build(
         ckpt_dir=ckpt_dir,
         tokenizer_path=tokenizer_path,
         max_seq_len=max_seq_len,
         max_batch_size=max_batch_size,
+        model_parallel_size=1
     )
 
-    dialog = [
-        {'role': 'system', 'content': 'Echo the user\'s message back.'},
-        {'role': 'user', 'content': 'Hello!'},
-        {'role': 'user', 'content': 'What is the capital of France?'}
-    ]
+    system, q1, q2 = workflow.insert([
+        {'role': 'system', 'content': 'Answer the user\'s question.'},
+        {'role': 'user', 'content': 'What is the capital of France?'},
+        {'role': 'user', 'content': 'What is the capital of Germany?'},
+    ])
 
-    system, user_1, user_2 = workflow.insert(dialog)
-
-    outputs, *_ = workflow.step(
-        [
-            {'requirements': [system, user_1], 'expects': ('assistant', None)},
-            {'requirements': [system, user_1], 'expects': ('assistant', None)},
-        ]
-        max_gen_len, temperature, top_p, log_probs
+    tokens, ids, _ = workflow.step(
+        tasks=[
+            {'requirements': [system, q1], 'expects': ('assistant', None)},
+            {'requirements': [system, q1], 'expects': ('assistant', None)},
+            {'requirements': [system, q2], 'expects': ('assistant', None)},
+            {'requirements': [system, q2], 'expects': ('assistant', None)},
+        ],
+        max_gen_len=32,
+        temperature=1.0,
+        top_p=0.9,
+        prefill=True,
+        seed=1,
     )
 
-    outputs, *_ = workflow.step(
-        [
-            {'requirements': [system, user_2], 'expects': ('assistant', None)},
-            {'requirements': [system, user_2], 'expects': ('assistant', None)},
-        ]
-        max_gen_len, temperature, top_p, log_probs
-    )
-
-    outputs, *_ = workflow.step(
-        [
-            {'requirements': [system, user_1], 'expects': ('assistant', None)},
-            {'requirements': [system, user_2], 'expects': ('assistant', None)},
-        ]
-        max_gen_len, temperature, top_p, log_probs
-    )
+    for i, output in enumerate(tokens):
+        print(f'example #{i}')
+        print(workflow.tokenizer.decode(output))
 
 if __name__ == '__main__':
     fire.Fire(main)
