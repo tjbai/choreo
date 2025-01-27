fe0e87f6d0abb87d988811c7c18cc0c98ba86658
TJ Bai <tjianbai@gmail.com>
2025-01-27 17:00:27 -0500

    extract load model and tokenizer to util

--- llama/generation.py
+++ llama/generation.py
@@ -1,25 +1,15 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
 
-import json
-import os
-import sys
-import time
-from pathlib import Path
-from typing import List, Optional, Tuple, TypedDict, Sequence
+from typing import List, Optional, Tuple, TypedDict
 from typing_extensions import Required
 
 import torch
 import torch.nn.functional as F
-from fairscale.nn.model_parallel.initialize import (
-    get_model_parallel_rank,
-    initialize_model_parallel,
-    model_parallel_is_initialized,
-)
 
-from llama.model import ModelArgs, Transformer
+from llama.model import Transformer
 from llama.tokenizer import ChatFormat, Dialog, Message, Tokenizer
-
+from llama.util import load_model_and_tokenizer
 
 class CompletionPrediction(TypedDict, total=False):
     generation: Required[str]
@@ -43,75 +33,16 @@ class Llama:
         model_parallel_size: Optional[int] = None,
         seed: int = 1,
     ) -> "Llama":
-        """
-        Build a Llama instance by initializing and loading a model checkpoint.
-
-        Args:
-            ckpt_dir (str): Path to the directory containing checkpoint files.
-            tokenizer_path (str): Path to the tokenizer file.
-            max_seq_len (int): Maximum sequence length for input text.
-            max_batch_size (int): Maximum batch size for inference.
-            model_parallel_size (Optional[int], optional): Number of model parallel processes.
-                If not provided, it's determined from the environment. Defaults to None.
-
-        Returns:
-            Llama: An instance of the Llama class with the loaded model and tokenizer.
-
-        Raises:
-            AssertionError: If there are no checkpoint files in the specified directory,
-                or if the model parallel size does not match the number of checkpoint files.
-
-        Note:
-            This method initializes the distributed process group, sets the device to CUDA,
-            and loads the pre-trained model and tokenizer.
-        """
-        assert 1 <= max_seq_len <= 8192, f"max_seq_len must be between 1 and 8192, got {max_seq_len}."
-        assert os.path.isdir(ckpt_dir), f"Checkpoint directory '{ckpt_dir}' does not exist."
-        assert os.path.isfile(tokenizer_path), f"Tokenizer file '{tokenizer_path}' does not exist."
-
-        if not torch.distributed.is_initialized():
-            torch.distributed.init_process_group("nccl")
-        if not model_parallel_is_initialized():
-            if model_parallel_size is None:
-                model_parallel_size = int(os.environ.get("WORLD_SIZE", 1))
-            initialize_model_parallel(model_parallel_size)
-
-        local_rank = int(os.environ.get("LOCAL_RANK", 0))
-        torch.cuda.set_device(local_rank)
-
-        # seed must be the same in all processes
-        torch.manual_seed(seed)
-
-        if local_rank > 0:
-            sys.stdout = open(os.devnull, "w")
-
-        start_time = time.time()
-        checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
-        assert len(checkpoints) > 0, f"no checkpoint files found in {ckpt_dir}"
-        assert model_parallel_size == len(
-            checkpoints
-        ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {model_parallel_size}"
-        ckpt_path = checkpoints[get_model_parallel_rank()]
-        checkpoint = torch.load(ckpt_path, map_location="cpu", weights_only=True)
-        with open(Path(ckpt_dir) / "params.json", "r") as f:
-            params = json.loads(f.read())
-
-        model_args: ModelArgs = ModelArgs(
+        model, tokenizer = load_model_and_tokenizer(
+            ckpt_dir=ckpt_dir,
+            tokenizer_path=tokenizer_path,
             max_seq_len=max_seq_len,
             max_batch_size=max_batch_size,
-            **params,
+            model_class=Transformer,
+            model_parallel_size=model_parallel_size,
+            seed=seed,
+            strict=True
         )
-        tokenizer = Tokenizer(model_path=tokenizer_path)
-        assert model_args.vocab_size == tokenizer.n_words
-        if torch.cuda.is_bf16_supported():
-            torch.set_default_device('cuda')
-            torch.set_default_dtype(torch.bfloat16)
-        else:
-            torch.set_default_device('cuda')
-            torch.set_default_dtype(torch.float16)
-        model = Transformer(model_args).cuda()
-        model.load_state_dict(checkpoint, strict=False)
-        print(f"Loaded in {time.time() - start_time:.2f} seconds")
 
         return Llama(model, tokenizer)
 
---
+++ llama/util.py
@@ -0,0 +1,101 @@
+import os
+import sys
+import time
+import json
+from pathlib import Path
+from typing import Type, Optional, Tuple
+
+import torch
+from fairscale.nn.model_parallel.initialize import (
+    get_model_parallel_rank,
+    initialize_model_parallel,
+    model_parallel_is_initialized,
+)
+
+from llama.model import Transformer, ModelArgs
+from llama.tokenizer import Tokenizer
+
+def load_model_and_tokenizer(
+    ckpt_dir: str,
+    tokenizer_path: str,
+    max_seq_len: int,
+    max_batch_size: int,
+    model_class: Type[Transformer] = Transformer,
+    model_parallel_size: Optional[int] = None,
+    seed: int = 1,
+    strict: bool = True,
+) -> Tuple[Transformer, Tokenizer]:
+    """
+    Build a Llama instance by initializing and loading a model checkpoint.
+
+    Args:
+        ckpt_dir (str): Path to the directory containing checkpoint files.
+        tokenizer_path (str): Path to the tokenizer file.
+        max_seq_len (int): Maximum sequence length for input text.
+        max_batch_size (int): Maximum batch size for inference.
+        model_parallel_size (Optional[int], optional): Number of model parallel processes.
+            If not provided, it's determined from the environment. Defaults to None.
+
+    Returns:
+        Llama: An instance of the Llama class with the loaded model and tokenizer.
+
+    Raises:
+        AssertionError: If there are no checkpoint files in the specified directory,
+            or if the model parallel size does not match the number of checkpoint files.
+
+    Note:
+        This method initializes the distributed process group, sets the device to CUDA,
+        and loads the pre-trained model and tokenizer.
+    """
+    assert 1 <= max_seq_len <= 8192
+    assert os.path.isdir(ckpt_dir)
+    assert os.path.isfile(tokenizer_path)
+
+    if not torch.distributed.is_initialized():
+        torch.distributed.init_process_group("nccl")
+
+    if not model_parallel_is_initialized():
+        if model_parallel_size is None:
+            model_parallel_size = int(os.environ.get("WORLD_SIZE", 1))
+        initialize_model_parallel(model_parallel_size)
+
+    local_rank = int(os.environ.get("LOCAL_RANK", 0))
+    torch.cuda.set_device(local_rank)
+    torch.manual_seed(seed)
+    if local_rank > 0:
+        sys.stdout = open(os.devnull, "w")
+
+    start_time = time.time()
+    checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+    assert len(checkpoints) > 0, f"no checkpoint files found in {ckpt_dir}"
+
+    assert model_parallel_size == len(checkpoints), \
+        f"Loading a checkpoint for MP={len(checkpoints)} but world size is {model_parallel_size}"
+
+    ckpt_path = checkpoints[get_model_parallel_rank()]
+    checkpoint = torch.load(ckpt_path, map_location="cpu", weights_only=True)
+
+    with open(Path(ckpt_dir) / "params.json", "r") as f:
+        params = json.loads(f.read())
+
+    model_args = ModelArgs(
+        max_seq_len=max_seq_len,
+        max_batch_size=max_batch_size,
+        **params,
+    )
+
+    tokenizer = Tokenizer(model_path=tokenizer_path)
+    assert model_args.vocab_size == tokenizer.n_words
+
+    if torch.cuda.is_bf16_supported():
+        torch.set_default_device('cuda')
+        torch.set_default_dtype(torch.bfloat16)
+    else:
+
+        torch.set_default_dtype(torch.float16)
+
+    model = model_class(model_args).cuda()
+    model.load_state_dict(checkpoint, strict=strict)
+    print(f"Loaded in {time.time() - start_time:.2f} seconds")
+
+    return model, tokenizer
