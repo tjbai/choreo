f70084fc26fc5f1f0053e89e8659379eeda9c936
tjbai <tjianbai@gmail.com>
2025-02-04 17:20:17 -0500

    organize dumps (need better naming) and return e2e with new checkpoints

--- slurm/math_e2e.py
+++ slurm/math_e2e.py
@@ -26,21 +26,26 @@ import torch
 from tqdm import tqdm
 from llama.workflows.tot import load_math_problems, tot_cached, tot_baseline
 
-# full split is just 280 problems, might as well do them all
 problems = load_math_problems('/home/tbai4/llama3/data/MATH', split='val')
 
-for id in [0, 99, 199, 299, 399]:
-    if id == 0:
-        workflow.model.set_adapter_state(enabled=False)
-    else:
-        workflow.model.set_adapter_state(enabled=True)
-        checkpoint = torch.load(f'/scratch4/jeisner1/tjbai/checkpoints/lora_epoch-0_step-{id}.pt', weights_only=True)
-        workflow.model.load_state_dict(checkpoint['lora'])
-    print(f'Loaded checkpoint-{id}')
-
+for ckpt_path in [
+   # "lora_epoch-0_step-195.pt",
+   # "lora_epoch-0_step-395.pt",
+   "lora_epoch-0_step-595.pt",
+   # "lora_epoch-0_step-795.pt",
+   "lora_epoch-1_step-95.pt",
+   # "lora_epoch-1_step-295.pt",
+   "lora_epoch-1_step-495.pt",
+   # "lora_epoch-1_step-695.pt",
+   "lora_epoch-1_step-895.pt",
+]:
+    workflow.model.set_adapter_state(enabled=True)
+    checkpoint = torch.load(f'/scratch4/jeisner1/tjbai/checkpoints/tot_2/{ckpt_path}', weights_only=True)
+    workflow.model.load_state_dict(checkpoint['lora'])
     workflow.model.reshape_cache(1)
+
     comps = []
-    for problem in tqdm(problems, desc=f'checkpoint {id}'):
+    for problem in tqdm(problems, desc=f'checkpoint {ckpt_path}'):
         workflow.reset()
         comps.append(tot_cached(
             workflow=workflow,
@@ -48,19 +53,6 @@ for id in [0, 99, 199, 299, 399]:
             branching_factor=8,
             voters=4,
         ))
-    with open(f'checkpoint-{id}_e2e.json', 'w') as f:
-        json.dump(comps, f)
 
-    if id == 0:
-        llama = Llama(workflow.model, workflow.tokenizer)
-        llama.model.reshape_cache(8)
-        comps = []
-        for problem in tqdm(problems, desc='baseline'):
-            comps.append(tot_baseline(
-                llama=llama,
-                problem=problem['problem'],
-                branching_factor=8,
-                voters=4
-            ))
-        with open('baseline_e2e.json', 'w') as f:
-            json.dump(comps, f)
+    with open(f'{ckpt_path}_e2e.json', 'w') as f:
+        json.dump(comps, f)
