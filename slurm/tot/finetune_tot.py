62992ac70371f4368f78bc7119f42be19b036af4
tjbai <tjianbai@gmail.com>
2025-03-05 15:32:39 -0500

    tot b=8,v=8 shuffled and unshuffled baselines

--- slurm/tot/finetune_tot.py
+++ slurm/tot/finetune_tot.py
@@ -4,13 +4,13 @@ finetune(
     data_path='/home/tbai4/llama3/llama/tot_data_2',
     ckpt_dir='/scratch4/jeisner1/tjbai/llama_8b',
     tokenizer_path='/scratch4/jeisner1/tjbai/llama_8b/tokenizer.model',
-    output_dir='/scratch4/jeisner1/tjbai/checkpoints/tot_3',
+    output_dir='/scratch4/jeisner1/tjbai/checkpoints/tot_4',
     gradient_accumulation_steps=4,
     max_seq_len=8192,
     checkpoint_freq=50,
     validation_freq=100,
     branching_factor=8,
-    voters=4,
+    voters=8,
     epochs=2,
     use_lora=True,
     lora_rank=64,
@@ -18,3 +18,4 @@ finetune(
     lora_dropout=0.05,
     learning_rate=5e-5,
 )
+
