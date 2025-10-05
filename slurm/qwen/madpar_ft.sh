7df7bfa01c93e719bcfc1b76b832ae67bff99efe
tjbai <tjianbai@gmail.com>
2025-10-04 21:24:34 -0400

    madpar ft

---
+++ slurm/qwen/madpar_ft.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=madpar_ft
+#SBATCH -A jeisner1_gpu
+#SBATCH --partition=ica100
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=80G
+#SBATCH --time=12:00:00
+#SBATCH --output=madpar_ft.out
+
+uv run python slurm/qwen/ft.py --task madpar
